fa4e697b844567a37a03672dfe7bdecc8c7ae66c
Jose <ayenijoseph45@gmail.com>
2025-07-07 21:37:18 +0100

    modify the auth files

---
+++ api/authentication/login.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class Login(BaseModel):
+    username: str
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: Optional[str]= None
+
+
+model_config = {
+    "from_attributes = True"
+}    
+
+    
